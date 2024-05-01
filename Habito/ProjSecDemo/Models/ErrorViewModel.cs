0a890f57d6ca7e2eb36c04c313b7c86a3f69d0e2
Theotim Chevalier <133657171+TheotimChevalier@users.noreply.github.com>
2024-05-01 23:28:36 +0200

    Add files via upload

---
+++ Habito/ProjSecDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjSecDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Habito/ProjSecDemo/Models/ProductEditViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace ProjSecDemo.Models
+{
+    public class ProductEditViewModel
+    {
+       
+            public int id { get; set; }
+            public string Titre { get; set; } 
+            public string Description { get; set; } 
+            public string Fabricant { get; set; } 
+            public string InfoSup { get; set; } 
+            public int Prix_Vente { get; set; }
+            public string Type { get; set; }
+            public bool DroitEdit { get; set; }
+
+    }
+
+}
